e63d838c8268a588560b31f5999c8540ebfa9815
Jacqueline123 <Jacqueline.grapes@yandex.ru>
2016-05-02 23:31:09 +0300

    Create script.js
    just a script

---
+++ script.js
@@ -0,0 +1,11 @@
+# May_2016
+<script type="text/javascript">
+var rotation = function (){
+   jQuery("#rotateImg").rotate({
+      angle:0, 
+      animateTo:360, 
+      callback: rotation
+   });
+}
+rotation();
+<script>
