39c361a1563106705856770b78d9b0332016b5e3
Jacqueline123 <Jacqueline.grapes@yandex.ru>
2016-05-06 00:00:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 # May_2016
 <script type="text/javascript">
-var rotation = function (){
+var rotation1 = function (){
    jQuery("#rotateImg").rotate({
       angle:0, 
       animateTo:180, 
       callback: rotation
    });
 }
-rotation();
+rotation1();
 <script>
